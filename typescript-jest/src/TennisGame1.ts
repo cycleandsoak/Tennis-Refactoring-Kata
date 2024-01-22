a4f4af0320748ac5241419409e63cb140f217912
Centralus ACC2 <A2CFeaturesTeam1User@hunterindustries.com>
2024-01-22 09:32:31 -0800

    r - extract variables and methods

--- typescript-jest/src/TennisGame1.ts
+++ typescript-jest/src/TennisGame1.ts
@@ -1,75 +1,84 @@
-import { TennisGame } from './TennisGame';
+import {TennisGame} from './TennisGame';
 
 export class TennisGame1 implements TennisGame {
-  private m_score1: number = 0;
-  private m_score2: number = 0;
-  private player1Name: string;
-  private player2Name: string;
+    private m_score1: number = 0;
+    private m_score2: number = 0;
+    private player1Name: string;
+    private player2Name: string;
 
-  constructor(player1Name: string, player2Name: string) {
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
-  }
-
-  wonPoint(playerName: string): void {
-    if (playerName === 'player1')
-      this.m_score1 += 1;
-    else
-      this.m_score2 += 1;
-  }
-
-  getScore(): string {
-    let score: string = '';
-    let tempScore: number = 0;
-    let playerScoresAreEqual = this.m_score1 === this.m_score2;
-    if (playerScoresAreEqual) {
-      score = this.computeScoreIfBothAreTied(score);
+    constructor(player1Name: string, player2Name: string) {
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
-    else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-      const differenceBetweenPlayerScores: number = this.m_score1 - this.m_score2;
-      if (differenceBetweenPlayerScores === 1) score = 'Advantage player1';
-      else if (differenceBetweenPlayerScores === -1) score = 'Advantage player2';
-      else if (differenceBetweenPlayerScores >= 2) score = 'Win for player1';
-      else score = 'Win for player2';
+
+    wonPoint(playerName: string): void {
+        if (playerName === 'player1')
+            this.m_score1 += 1;
+        else
+            this.m_score2 += 1;
     }
-    else {
-      for (let i = 1; i < 3; i++) {
-        if (i === 1) tempScore = this.m_score1;
-        else { score += '-'; tempScore = this.m_score2; }
-        switch (tempScore) {
-          case 0:
-            score += 'Love';
-            break;
-          case 1:
-            score += 'Fifteen';
-            break;
-          case 2:
-            score += 'Thirty';
-            break;
-          case 3:
-            score += 'Forty';
-            break;
+
+    getScore(): string {
+        let score: string = '';
+        let tempScore: number = 0;
+        let playerScoresAreEqual = this.m_score1 === this.m_score2;
+        let isMatchPoint = this.m_score1 >= 4 || this.m_score2 >= 4;
+
+        if (playerScoresAreEqual) {
+            score = this.computeScoreIfBothAreTied(score);
+        } else {
+            if (isMatchPoint) {
+                score = this.computeScoreIfMatchPoint();
+            } else {
+                for (let i = 1; i < 3; i++) {
+                    if (i === 1) tempScore = this.m_score1;
+                    else {
+                        score += '-';
+                        tempScore = this.m_score2;
+                    }
+                    switch (tempScore) {
+                        case 0:
+                            score += 'Love';
+                            break;
+                        case 1:
+                            score += 'Fifteen';
+                            break;
+                        case 2:
+                            score += 'Thirty';
+                            break;
+                        case 3:
+                            score += 'Forty';
+                            break;
+                    }
+                }
+            }
         }
-      }
+        return score;
     }
-    return score;
-  }
 
-  private computeScoreIfBothAreTied(score: string) {
-    switch (this.m_score1) {
-      case 0:
-        score = 'Love-All';
-        break;
-      case 1:
-        score = 'Fifteen-All';
-        break;
-      case 2:
-        score = 'Thirty-All';
-        break;
-      default:
-        score = 'Deuce';
-        break;
+    private computeScoreIfMatchPoint() {
+        const differenceBetweenPlayerScores: number = this.m_score1 - this.m_score2;
+        if (differenceBetweenPlayerScores === 1) return 'Advantage player1';
+        else if (differenceBetweenPlayerScores === -1) return 'Advantage player2';
+        else if (differenceBetweenPlayerScores >= 2) return 'Win for player1';
+        else return 'Win for player2';
     }
-    return score;
-  }
+
+    private computeScoreIfBothAreTied(score: string) {
+        switch (this.m_score1) {
+            case 0:
+                score = 'Love-All';
+                break;
+            case 1:
+                score = 'Fifteen-All';
+                break;
+            case 2:
+                score = 'Thirty-All';
+                break;
+            default:
+                score = 'Deuce';
+                break;
+        }
+        return score;
+    }
 }
